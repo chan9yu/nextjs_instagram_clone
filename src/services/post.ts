ed14c898c0ffb720b9e55b7712a472a48c871e8d
chan9yu <wry5533@gmail.com>
2023-12-10 16:19:35 +0900

    feat: user · post service 로직 추가

--- src/services/post.ts
+++ src/services/post.ts
@@ -13,6 +13,14 @@ const simplePostProjection = `
   "createdAt": _createdAt
 `;
 
+function mapPosts(posts: SimplePost[]) {
+	return posts.map((post: SimplePost) => ({
+		...post,
+		likes: post.likes ?? [],
+		image: urlFor(post.image)
+	}));
+}
+
 export async function getFollowingPostsOf(username: string) {
 	return client
 		.fetch(
@@ -22,7 +30,7 @@ export async function getFollowingPostsOf(username: string) {
         ${simplePostProjection}
       }`
 		)
-		.then((posts: SimplePost[]) => posts.map(post => ({ ...post, image: urlFor(post.image) })));
+		.then(mapPosts);
 }
 
 export async function getPost(id: string): Promise<FullPost> {
@@ -48,3 +56,94 @@ export async function getPost(id: string): Promise<FullPost> {
 			image: urlFor(post.image)
 		}));
 }
+
+export async function getPostsOf(username: string) {
+	return client
+		.fetch(
+			`*[_type == "post" && author -> username == "${username}"]
+      | order(_createdAt desc){${simplePostProjection}}`
+		)
+		.then(mapPosts);
+}
+
+export async function getLikedPostsOf(username: string) {
+	return client
+		.fetch(
+			`*[_type == "post" && "${username}" in likes[] -> username]
+      | order(_createdAt desc){${simplePostProjection}}`
+		)
+		.then(mapPosts);
+}
+
+export async function getSavedPostsOf(username: string) {
+	return client
+		.fetch(
+			`*[_type == "post" && _id in *[_type == "user" && username == "${username}"].bookmarks[]._ref]
+      | order(_createdAt desc){${simplePostProjection}}`
+		)
+		.then(mapPosts);
+}
+
+export async function likePost(postId: string, userId: string) {
+	return client
+		.patch(postId)
+		.setIfMissing({ likes: [] })
+		.append('likes', [
+			{
+				_ref: userId,
+				_type: 'reference'
+			}
+		])
+		.commit({ autoGenerateArrayKeys: true });
+}
+
+export async function dislikePost(postId: string, userId: string) {
+	return client
+		.patch(postId)
+		.unset([`likes[_ref=="${userId}"]`])
+		.commit();
+}
+
+export async function addComment(postId: string, userId: string, comment: string) {
+	return client
+		.patch(postId)
+		.setIfMissing({ comments: [] })
+		.append('comments', [
+			{
+				comment,
+				author: { _ref: userId, _type: 'reference' }
+			}
+		])
+		.commit({ autoGenerateArrayKeys: true });
+}
+
+// export async function createPost(userId: string, text: string, file: Blob) {
+// 	console.log(userId, text, file);
+
+// 	return fetch(assetsURL, {
+// 		method: 'POST',
+// 		headers: {
+// 			'content-type': file.type,
+// 			authorization: `Bearer ${process.env.SANITY_SECRET_TOKEN}`
+// 		},
+// 		body: file
+// 	})
+// 		.then(res => res.json())
+// 		.then(result => {
+// 			return client.create(
+// 				{
+// 					_type: 'post',
+// 					author: { _ref: userId },
+// 					photo: { asset: { _ref: result.document._id } },
+// 					comments: [
+// 						{
+// 							comment: text,
+// 							author: { _ref: userId, _type: 'reference' }
+// 						}
+// 					],
+// 					likes: []
+// 				},
+// 				{ autoGenerateArrayKeys: true }
+// 			);
+// 		});
+// }
--- src/services/user.ts
+++ src/services/user.ts
@@ -19,12 +19,12 @@ export async function addUser({ email, id, name, username, image }: OAuthUser) {
 export async function getUserByUsername(username: string) {
 	return client.fetch(
 		`*[_type == "user" && username == "${username}"][0]{
-			...,
-			"id": _id,
-			following[] -> { username, image },
-			followers[] -> { username, image },
-			"bookmarks": bookmarks[] -> _id
-		}`
+      ...,
+      "id": _id,
+      following[] -> { username, image },
+      followers[] -> { username, image },
+      "bookmarks": bookmarks[] -> _id
+    }`
 	);
 }
 
@@ -33,10 +33,10 @@ export async function searchUsers(keyword?: string): Promise<SearchUser[]> {
 	return client
 		.fetch(
 			`*[_type == "user" ${query}]{
-				...,
-				"following": count(following),
-				"followers": count(followers),
-			}`
+      ...,
+      "following": count(following),
+      "followers": count(followers),
+    }`
 		)
 		.then((users: SearchUser[]) =>
 			users.map(user => ({
@@ -51,12 +51,12 @@ export async function getUserForProfile(username: string) {
 	return client
 		.fetch(
 			`*[_type == "user" && username == "${username}"][0]{
-				...,
-				"id": _id,
-				"following": count(following),
-				"followers": count(followers),
-				"posts": count(*[_type == "post" && author -> username == "${username}"])
-			}`
+      ...,
+      "id": _id,
+      "following": count(following),
+      "followers": count(followers),
+      "posts": count(*[_type == "post" && author -> username == "${username}"])
+    }`
 		)
 		.then(user => ({
 			...user,
@@ -64,4 +64,44 @@ export async function getUserForProfile(username: string) {
 			followers: user.followers ?? 0,
 			posts: user.posts ?? 0
 		}));
+}
+
+export async function addBookmark(userId: string, postId: string) {
+	return client
+		.patch(userId)
+		.setIfMissing({ bookmarks: [] })
+		.append('bookmarks', [
+			{
+				_ref: postId,
+				_type: 'reference'
+			}
+		])
+		.commit({ autoGenerateArrayKeys: true });
+}
+
+export async function removeBookmark(userId: string, postId: string) {
+	return client
+		.patch(userId)
+		.unset([`bookmarks[_ref == "${postId}"]`])
+		.commit();
+}
+
+export async function follow(myId: string, targetId: string) {
+	return client
+		.transaction()
+		.patch(myId, user =>
+			user.setIfMissing({ following: [] }).append('following', [{ _ref: targetId, _type: 'reference' }])
+		)
+		.patch(targetId, user =>
+			user.setIfMissing({ followers: [] }).append('followers', [{ _ref: myId, _type: 'reference' }])
+		)
+		.commit({ autoGenerateArrayKeys: true });
+}
+
+export async function unfollow(myId: string, targetId: string) {
+	return client
+		.transaction()
+		.patch(myId, user => user.unset([`following[_ref == "${targetId}"]`]))
+		.patch(targetId, user => user.unset([`followers[_ref == "${myId}"]`]))
+		.commit({ autoGenerateArrayKeys: true });
 }
